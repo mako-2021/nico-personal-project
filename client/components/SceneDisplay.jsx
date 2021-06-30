1d13e75681d41fcda12c78e9464b3f8e24c6bed7
nicolas hayvice-caballe <nicolashayvice@gmail.com>
2021-07-01 09:11:08 +1200

    initial setup complete for showing

--- client/components/SceneDisplay.jsx
+++ client/components/SceneDisplay.jsx
@@ -5,36 +5,27 @@ import SceneComponent from './SceneComponent'
 let box
 
 const onSceneReady = (scene) => {
-  // This creates and positions a free camera (non-mesh)
-  var camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene)
+  const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene)
 
-  // This targets the camera to scene origin
   camera.setTarget(Vector3.Zero())
 
   const canvas = scene.getEngine().getRenderingCanvas()
-
-  // This attaches the camera to the canvas
   camera.attachControl(canvas, true)
 
-  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  var light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
+  // attaches the cameera to the canvas
+  const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
 
-  // Default intensity is 1. Let's dim the light a small amount
-  light.intensity = 0.7
+  light.intensity = 0.7 // average light is 1
 
-  // Our built-in 'box' shape.
-  box = MeshBuilder.CreateBox('box', { size: 2 }, scene)
+  // pre built babylonjs builds
+  box = MeshBuilder.CreateBox('box', { size: 5 }, scene)
 
-  // Move the box upward 1/2 its height
   box.position.y = 1
 
-  // Our built-in 'ground' shape.
   MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene)
 }
 
-/**
- * Will run on every frame render.  We are spinning the box on y-axis.
- */
+// spins the box 
 // const onRender = (scene) => {
 //   if (box !== undefined) {
 //     var deltaTimeInMillis = scene.getEngine().getDeltaTime()
